62b6a06cf15cba399a45b35fe58783546492f7b6
Davide <Davide@Davide-TOSH.homenet.telecomitalia.it>
2017-07-06 12:45:44 +0200

    LibrettoController

--- src/it/polito/tdp/libretto/LibrettoController.java
+++ src/it/polito/tdp/libretto/LibrettoController.java
@@ -1,5 +1,61 @@
+/**
+ * Sample Skeleton for 'Libretto.fxml' Controller Class
+ */
+
 package it.polito.tdp.libretto;
 
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextArea;
+
 public class LibrettoController {
-	
+
+    @FXML // ResourceBundle that was given to the FXMLLoader
+    private ResourceBundle resources;
+
+    @FXML // URL location of the FXML file that was given to the FXMLLoader
+    private URL location;
+
+    @FXML // fx:id="txtCodice"
+    private Label txtCodice; // Value injected by FXMLLoader
+
+    @FXML // fx:id="txtTitolo"
+    private Label txtTitolo; // Value injected by FXMLLoader
+
+    @FXML // fx:id="txtDocente"
+    private Label txtDocente; // Value injected by FXMLLoader
+
+    @FXML // fx:id="btncerca"
+    private Button btncerca; // Value injected by FXMLLoader
+
+    @FXML // fx:id="btninserisci"
+    private Button btninserisci; // Value injected by FXMLLoader
+
+    @FXML // fx:id="txtMessage"
+    private TextArea txtMessage; // Value injected by FXMLLoader
+
+    @FXML
+    void handleCerca(ActionEvent event) {
+
+    }
+
+    @FXML
+    void handleInserisci(ActionEvent event) {
+
+    }
+
+    @FXML // This method is called by the FXMLLoader when initialization is complete
+    void initialize() {
+        assert txtCodice != null : "fx:id=\"txtCodice\" was not injected: check your FXML file 'Libretto.fxml'.";
+        assert txtTitolo != null : "fx:id=\"txtTitolo\" was not injected: check your FXML file 'Libretto.fxml'.";
+        assert txtDocente != null : "fx:id=\"txtDocente\" was not injected: check your FXML file 'Libretto.fxml'.";
+        assert btncerca != null : "fx:id=\"btncerca\" was not injected: check your FXML file 'Libretto.fxml'.";
+        assert btninserisci != null : "fx:id=\"btninserisci\" was not injected: check your FXML file 'Libretto.fxml'.";
+        assert txtMessage != null : "fx:id=\"txtMessage\" was not injected: check your FXML file 'Libretto.fxml'.";
+
+    }
 }
