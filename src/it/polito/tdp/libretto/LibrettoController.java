1c7fa430f83c18349ee5a3e94621e6d10796f3e8
Davide <Davide@Davide-TOSH.homenet.telecomitalia.it>
2017-07-06 15:47:48 +0200

    Fine progretto

--- src/it/polito/tdp/libretto/LibrettoController.java
+++ src/it/polito/tdp/libretto/LibrettoController.java
@@ -6,6 +6,9 @@ package it.polito.tdp.libretto;
 
 import java.net.URL;
 import java.util.ResourceBundle;
+
+import it.polito.tdp.libretto.model.Esame;
+import it.polito.tdp.libretto.model.Model;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
@@ -13,6 +16,9 @@ import javafx.scene.control.Label;
 import javafx.scene.control.TextArea;
 
 public class LibrettoController {
+	
+	Model model;
+	
 
     @FXML // ResourceBundle that was given to the FXMLLoader
     private ResourceBundle resources;
@@ -40,11 +46,53 @@ public class LibrettoController {
 
     @FXML
     void handleCerca(ActionEvent event) {
+    	String codice = txtCodice.getText();
+    	
+    	if(codice.length()<5) {
+    		txtMessage.appendText("Codice corso non valido\n");
+    		return;	
+    	}//se è valido
+    	
+    	Esame e = model.trovaEsame(codice);
+    	
+    	if (e==null){
+    		txtMessage.appendText("Codice "+codice+" non trovato\n");
+    		return;
+    	} else {
+    		txtMessage.appendText("Codice "+codice+" trovato\n");
+
+
+    		txtCodice.setText(e.getCodice());
+    		txtTitolo.setText(e.getTitolo());
+    		txtDocente.setText(e.getDocente());
+    	}
+    	
 
     }
 
     @FXML
     void handleInserisci(ActionEvent event) {
+    	//recupera i dati dalla vista
+    	String codice = txtCodice.getText();
+    	String titolo = txtTitolo.getText();
+    	String docente = txtDocente.getText();
+    	
+    	//verifica la validità dei dati
+    	if( codice.length()<5 || titolo.length()==0 || docente.length()==0){
+    		txtMessage.appendText("dati esame insufficienti\n");
+    		return;
+    	}
+    	
+    	//chiedi al Model di effettuare l'operazione
+    	Esame e = new Esame(codice, titolo, docente) ;
+    	boolean result = model.addEsame(e);
+    	    	
+    	//aggiorna la vista con il risultato dell'operazione
+    	if(result){
+    		txtMessage.appendText("Esame aggiunto correttamente\n");
+    	}else{
+    		txtMessage.appendText("Esame NON AGGIUNTO ( codice duplicato);\n");
+    	}
 
     }
 
@@ -58,4 +106,11 @@ public class LibrettoController {
         assert txtMessage != null : "fx:id=\"txtMessage\" was not injected: check your FXML file 'Libretto.fxml'.";
 
     }
+
+	/**
+	 * @param model the model to set
+	 */
+	public void setModel(Model model) {
+		this.model = model;
+	}
 }
